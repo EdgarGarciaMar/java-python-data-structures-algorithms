ddd9d73b769e5f1e9dda9e1055c4aee312ab3a20
Edgar García <edgargarcia092010@gmail.com>
2026-09-07 18:49:38 -0600

    Create set.py

---
+++ pythonset/set.py
@@ -0,0 +1,14 @@
+"""
+Set
+Sets are used to store multiple items in a single variable.
+
+Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+
+A set is a collection which is unordered, unchangeable*, and unindexed.
+
+https://www.w3schools.com/python/python_sets.asp
+
+"""
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
